fd7736b471fbb2ba60a4e80f364271f86ff681cc
AlexRobbin <158841503+AlexRobbin@users.noreply.github.com>
2024-02-06 22:02:42 +0330

    convertor code...third day practice

---
+++ convertor.py
@@ -0,0 +1,14 @@
+#...............converting km to mile...........
+'''
+print("how many km?")
+
+kms = input()
+
+mile = float(kms) * 0.621371
+mile = round(mile , 2)
+
+print("you import " + kms + "and converting to miles equales : " + str(mile))
+'''
+#................end...............................................................
+
+
